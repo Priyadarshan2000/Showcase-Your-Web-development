45a1f8953076ae286b8216316afa8e167412fb23
sayHack044 <117004605+sayHack044@users.noreply.github.com>
2022-10-30 20:28:20 +0530

    Update todoscript.js

--- todo-list/todoscript.js
+++ todo-list/todoscript.js
@@ -57,7 +57,7 @@ function updateLS() {
     todosEl.forEach(todoEl => {
         todos.push({
             text: todoEl.innerText,
-            completed: todoEl.classList.contains('completed')
+            completed: todoEl.classList.contains('done it')
         })
     })
 
